30a6c4f14fde995ac7e54fa17e964ea953cc0b52
ethan <ethanmalan10@gmail.com>
2026-04-06 21:06:14 -0600

    Merge branch 'main' of https://github.com/ethanmalan10/Intex-2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    port: 8080,
+    host: '0.0.0.0',
+    allowedHosts: ['intex-2-production.up.railway.app', 'all']
+  },
   server: {
     port: 5173,
-  },
+    host: '0.0.0.0'
+  }
 })
