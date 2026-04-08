4771567a77a368c6e8feb64f717e4d900b823d37
DarthJarJar3 <calebscaten@gmail.com>
2026-04-08 14:19:28 -0600

    Able to add conferences now

---
+++ backend/Models/CaseConference.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models;
+
+public class CaseConference
+{
+    [Key]
+    public int ConferenceId { get; set; }
+    public DateOnly ConferenceDate { get; set; }
+    public string Topic { get; set; } = string.Empty;
+    public string? Notes { get; set; }
+}
